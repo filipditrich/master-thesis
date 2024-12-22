75e1b1bd691e754e8d0bd2996786da58ecc0fb23
filipditrich <ditrich@nfctron.com>
2024-12-22 20:04:59 +0100

    chore(dashboard_app): move large queries to SQL functions instead
    Signed-off-by: filipditrich <ditrich@nfctron.com>

--- dashboard_app/_query_manager.py
+++ dashboard_app/_query_manager.py
@@ -11,7 +11,11 @@ query_manager = QueryManager(get_db_config())
 query_manager.registry.register_query(
 	QueryDefinition(
 		name="sankey_diagram",
-		sql=QueryManager.read_sql_query("sankey_diagram.sql"),
+		sql=QueryManager.process_sql_query(
+			"""
+			SELECT * FROM get_sankey_diagram_data(:date_from$1, :date_to$2)
+			"""
+		),
 		parameters=[
 			QueryParameter("date_from", datetime.datetime),
 			QueryParameter("date_to", datetime.datetime)
@@ -24,7 +28,11 @@ query_manager.registry.register_query(
 query_manager.registry.register_query(
 	QueryDefinition(
 		name="time_series",
-		sql=QueryManager.read_sql_query("time_series.sql"),
+		sql=QueryManager.process_sql_query(
+			"""
+			SELECT * FROM get_time_series(:date_from$1, :date_to$2, :granularity_minutes$3, :day_start_hour$4)
+			"""
+		),
 		parameters=[
 			QueryParameter("date_from", datetime.datetime),
 			QueryParameter("date_to", datetime.datetime),
